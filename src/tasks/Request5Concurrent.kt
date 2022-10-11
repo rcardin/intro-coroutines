24b22901bf205480815b6dfeac1f857e5a2f3280
rcardin <riccardo.cardin@gmail.com>
2022-10-11 09:12:01 +0200

    Completed task 5

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -3,6 +3,19 @@ package tasks
 import contributors.*
 import kotlinx.coroutines.*
 
-suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
-}
+suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> =
+    coroutineScope {
+        val repos = service
+            .getOrgReposResponse(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
+
+        repos.map { repo ->
+            async(Dispatchers.Default) {
+                service
+                    .getRepoContributorsResponse(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+            }
+        }.awaitAll().flatten().aggregate()
+    }
